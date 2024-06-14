fd98e124ba63a8e62a6bf0bad6aa71540512e8f2
raii <raii.program@gmail.com>
2024-06-15 01:30:19 +0900

    Add VRChat world project files

---
+++ Assets/Scripts/LuaConsole.cs
@@ -0,0 +1,99 @@
+﻿
+using System;
+using System.Text;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class LuaConsole : UdonSharpBehaviour
+{
+    [SerializeField] InputField sourceField;
+    [SerializeField] Text outputText;
+    [SerializeField] GameObject runButton;
+    [SerializeField] GameObject stopButton;
+    [SerializeField] Lua lua;
+    [SerializeField] class_wasi_snapshot_preview1 wasi;
+    [SerializeField] string defaultSource;
+
+    [UdonSynced, FieldChangeCallback(nameof(Source))] string _source;
+    public string Source
+    {
+        get => _source;
+        set
+        {
+            _source = value;
+            sourceField.text = value;
+        }
+    }
+
+    int sourcePtr;
+
+    bool _running;
+    bool Running
+    {
+        get => _running;
+        set
+        {
+            _running = value;
+            runButton.SetActive(!value);
+            stopButton.SetActive(value);
+        }
+    }
+
+    public long execTime = 50;
+
+    void Start()
+    {
+        if (defaultSource != "" && Networking.IsMaster)
+        {
+            Source = defaultSource;
+            SyncSource();
+        }
+    }
+
+    public void SyncSource()
+    {
+        Networking.SetOwner(Networking.LocalPlayer, gameObject);
+
+        Source = sourceField.text;
+        RequestSerialization();
+    }
+
+    public void Run()
+    {
+        outputText.text = "";
+
+        lua.wasi_snapshot_preview1 = wasi;
+        wasi.wasm = lua;
+        wasi.outputText = outputText;
+
+        lua.w2us_init();
+        lua._initialize();
+
+        var sourceBytes = Encoding.UTF8.GetBytes(Source);
+        sourcePtr = lua.malloc(sourceBytes.Length + 1);
+
+        byte[] memory = lua.memory;
+        sourceBytes.CopyTo(memory, sourcePtr);
+        memory[sourcePtr + sourceBytes.Length] = 0;
+
+        Running = true;
+    }
+
+    public void Stop()
+    {
+        Running = false;
+    }
+
+    void Update()
+    {
+        if (!Running) return;
+
+        lua.set_yield_time((DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() + execTime) * 1_000_000);
+        int ret = lua.run(sourcePtr);
+        if (ret != 1) Running = false;
+    }
+}
---
+++ Assets/Scripts/class_wasi_snapshot_preview1.cs
@@ -0,0 +1,81 @@
+﻿
+using System;
+using System.Text;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class class_wasi_snapshot_preview1 : UdonSharpBehaviour
+{
+    [NonSerialized] public UdonSharpBehaviour wasm;
+    [NonSerialized] public Text outputText;
+
+    public int clock_time_get(int id, long precision, int time_ptr)
+    {
+        byte[] memory = (byte[])wasm.GetProgramVariable("memory");
+        long time = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() * 1_000_000;
+        BitConverter.GetBytes(time).CopyTo(memory, time_ptr);
+        return 0;
+    }
+
+    public int fd_seek(int fd, long offset, int whence, int offset_out_ptr)
+    {
+        return 0;
+    }
+
+    public int fd_write(int fd, int iovs_ptr, int iovs_len, int nwritten_ptr)
+    {
+        byte[] memory = (byte[])wasm.GetProgramVariable("memory");
+        int nwritten = 0;
+
+        for (int i = 0; i < iovs_len; i++)
+        {
+            int ptr = iovs_ptr + i * 8;
+            int iov_ptr = BitConverter.ToInt32(memory, ptr);
+            int iov_len = BitConverter.ToInt32(memory, ptr + 4);
+
+            string str = Encoding.UTF8.GetString(memory, iov_ptr, iov_len);
+            outputText.text += str;
+            nwritten += iov_len;
+        }
+
+        BitConverter.GetBytes(nwritten).CopyTo(memory, nwritten_ptr);
+
+        return 0;
+    }
+
+    public int fd_read(int fd, int iovs_ptr, int iovs_len, int nread_ptr)
+    {
+        return 0;
+    }
+
+    public int fd_close(int fd)
+    {
+        return 0;
+    }
+
+    public int fd_fdstat_get(int fd, int fdstat_ptr)
+    {
+        return 0;
+    }
+
+    public void proc_exit(int exit_code)
+    {
+        Debug.LogError($"proc_exit({exit_code})");
+    }
+
+    public int environ_sizes_get(int environ_count, int environ_size)
+    {
+        byte[] memory = (byte[])wasm.GetProgramVariable("memory");
+        BitConverter.GetBytes(0).CopyTo(memory, environ_count);
+        BitConverter.GetBytes(0).CopyTo(memory, environ_size);
+        return 0;
+    }
+
+    public int environ_get(int environ, int environ_buf)
+    {
+        return 0;
+    }
+}
